560f448793359c22d81d47d26ed7ae54f60327da
lisa-chen-58 <lisa.hl.chen@gmail.com>
2022-08-28 03:19:18 -0700

    adding banner link

--- flask_app/controllers/items.py
+++ flask_app/controllers/items.py
@@ -2,6 +2,9 @@ from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.item import Item
 
+# BANNERS
+# https: // manytools.org/hacker-tools/ascii-banner/
+
 # Home Route
 
 
